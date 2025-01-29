19e534b79c5c961cf9293896aaa7cd43da5ae41d
Steve Traugott <stevegt@t7a.org>
2025-01-28 16:22:49 -0800

    Create interfaces-git.go, define package, add refactor comments
    Create interfaces-git.go, define package, comment import, add refactor notes
    
    summary of diff --git  a/x/interfaces-git/interfaces-git.go b/x/interfaces-git/interfaces-git.go
    
    - Add `x/interfaces-git/interfaces-git.go` file
    - Define `interfaces-git` package
    - Include commented import for `github.com/stevegt/goadapt`
    - Add comments about organizing interfaces and planning future refactoring

---
+++ x/interfaces-git/interfaces-git.go
@@ -0,0 +1,11 @@
+package interfaces-git
+
+import (
+	// . "github.com/stevegt/goadapt"
+)
+
+// XXX Putting all of the interfaces in one file like this is not
+// idiomatic Go.  We're doing it here while getting thoughts in order.
+// All of these interfaces will be moved to their caller's files in
+// the future.
+
