bd32cf3468b4854c81b0cb9ad249574777363c99
Steve Traugott <stevegt@t7a.org>
2025-01-28 16:23:08 -0800

    Rename package to interfaces_git, comment out goadapt, delete imports
    Rename package, comment out goadapt import, delete unused imports
    
    summary of diff --git  a/x/interfaces-git/interfaces-git.go b/x/interfaces-git/interfaces-git.go
    
    - Rename package from interfaces-git to interfaces_git
    - Comment out import of "github.com/stevegt/goadapt"
    - Remove unnecessary import block

--- x/interfaces-git/interfaces-git.go
+++ x/interfaces-git/interfaces-git.go
@@ -1,11 +1,8 @@
-package interfaces-git
+package interfaces_git
 
-import (
-	// . "github.com/stevegt/goadapt"
-)
+// . "github.com/stevegt/goadapt"
 
 // XXX Putting all of the interfaces in one file like this is not
 // idiomatic Go.  We're doing it here while getting thoughts in order.
 // All of these interfaces will be moved to their caller's files in
 // the future.
-
