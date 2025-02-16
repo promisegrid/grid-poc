7bab4a110c46b87f3c952cdf7202a91a332e9010
Steve Traugott <stevegt@t7a.org>
2025-02-16 09:39:14 -0800

    add GopherJS demo
    Write a simple GopherJS demo that uses the DOM API to create a button
    that when clicked, adds a paragraph to the page that says "Hello,
    GopherJS!".

---
+++ x/gopherjs/hello/hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func main() {
+	// Get the global document object.
+	document := js.Global.Get("document")
+
+	// Create a button element.
+	button := document.Call("createElement", "button")
+	button.Set("innerHTML", "Click me")
+
+	// Append the button to the body.
+	document.Get("body").Call("appendChild", button)
+
+	// Add a click event listener that creates and appends a paragraph.
+	button.Call("addEventListener", "click", func() {
+		p := document.Call("createElement", "p")
+		p.Set("innerHTML", "Hello, GopherJS!")
+		document.Get("body").Call("appendChild", p)
+	})
+}
