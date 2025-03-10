2edeb47252a95efa52624da2decdfd01544ab687
Steve Traugott <stevegt@t7a.org>
2025-03-09 19:23:29 -0700

    use libp2p's DHT and Gossipsub to handle peer discovery automatically
    appears to work, needs input capabilities

--- x/gossipsub/gossipsub.go
+++ x/gossipsub/gossipsub.go
@@ -28,39 +28,38 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Create libp2p host with DHT and relay capabilities
+	// Get default bootstrap peers to be used for DHT bootstrapping and as static relays.
+	bootstrapPeers := dht.GetDefaultBootstrapPeerAddrInfos()
+
+	// Create libp2p host with DHT and relay capabilities.
 	h, err := libp2p.New(
 		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
 		libp2p.EnableRelay(),
-		libp2p.EnableAutoRelay(),
+		libp2p.EnableAutoRelayWithStaticRelays(bootstrapPeers),
 		libp2p.NATPortMap(),
 	)
 	if err != nil {
 		panic(err)
 	}
 
-	// Set up DHT with default IPFS bootstrap nodes
-	bootstrapPeers := dht.GetDefaultBootstrapPeerAddrInfos()
-	dhtMode := dht.Mode(
-		dht.ModeAuto,
-	)
+	// Set up DHT with default IPFS bootstrap nodes.
 	kademliaDHT, err := dht.New(ctx, h,
-		dhtMode,
+		dht.Mode(dht.ModeAuto),
 		dht.BootstrapPeers(bootstrapPeers...),
 	)
 	if err != nil {
 		panic(err)
 	}
 
-	// Bootstrap the DHT
+	// Bootstrap the DHT.
 	if err = kademliaDHT.Bootstrap(ctx); err != nil {
 		panic(err)
 	}
 
-	// Connect to bootstrap peers
+	// Connect to bootstrap peers.
 	connectToBootstrapPeers(ctx, h, bootstrapPeers)
 
-	// Set up Gossipsub with DHT-based peer discovery
+	// Set up Gossipsub with DHT-based peer discovery.
 	ps, err := pubsub.NewGossipSub(ctx, h,
 		pubsub.WithDiscovery(drouting.NewRoutingDiscovery(kademliaDHT)),
 		pubsub.WithPeerExchange(true),
@@ -69,37 +68,37 @@ func main() {
 		panic(err)
 	}
 
-	// Join the topic
+	// Join the topic.
 	topic, err := ps.Join(topicName)
 	if err != nil {
 		panic(err)
 	}
 
-	// Subscribe to topic
+	// Subscribe to topic.
 	sub, err := topic.Subscribe()
 	if err != nil {
 		panic(err)
 	}
 	defer sub.Cancel()
 
-	// Set up mDNS for local discovery
+	// Set up mDNS for local discovery.
 	setupMdnsDiscovery(ctx, h, rendezvous)
 
-	// Advertise our presence using DHT
+	// Advertise our presence using DHT.
 	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
 	routingDiscovery.Advertise(ctx, rendezvous)
 
-	// Start message handler
+	// Start message handler.
 	go handleMessages(ctx, sub)
 
-	// Print node information
+	// Print node information.
 	log.Printf("Node ID: %s\n", h.ID())
 	log.Printf("Connect using: /p2p-circuit/p2p/%s\n", h.ID())
 
-	// Maintain network connectivity
+	// Maintain network connectivity.
 	go discoverPeers(ctx, h, routingDiscovery)
 
-	// Wait for exit signal
+	// Wait for exit signal.
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	<-sigCh
